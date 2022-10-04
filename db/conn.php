daa0e8f1bc4c753976b5848e85564162bffe9f48
MelissaBarnett <78051622+MelissaBarnett90@users.noreply.github.com>
2022-10-03 23:26:19 -0500

    Set up Authentication Tables in database

--- db/conn.php
+++ db/conn.php
@@ -3,18 +3,18 @@
   
   
   //In development connection below
-  // $host = '127.0.0.1';
-   // $db = 'attendance_db';
-   // $user = 'root';
-    // $pass = '';
-   // $charset = 'ut8mb4';
+  $host = '127.0.0.1';
+  $db = 'attendance_db';
+  $user = 'root';
+  $pass = '';
+  $charset = 'ut8mb4';
 
    //remote database connection
-   $host = 'sql5.freemysqlhosting.net';
-   $db = 'sql5523795';
-   $user = 'sql5523795';
-   $pass = 'FmwSKn68GV';
-   $charset = 'ut8mb4';
+//    $host = 'sql5.freemysqlhosting.net';
+//    $db = 'sql5523795';
+//    $user = 'sql5523795';
+//    $pass = 'FmwSKn68GV';
+//    $charset = 'ut8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;$charset";
 
@@ -30,5 +30,10 @@
     }
 
     require_once 'crud.php';
+    require_once 'user.php';
     $crud = new crud($pdo);
-?>
+    $user = new user($pdo);
+
+    $user->insertUser("admin","password");
+
+    ?>
